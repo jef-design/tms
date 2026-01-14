426b0a688d172b11b8fd6606d62052a991d6e3fe
jef-design <bermejojff97@gmail.com>
2026-01-14 20:09:11 +0800

    home update

--- client/src/pages/ListDeliveries.tsx
+++ client/src/pages/ListDeliveries.tsx
@@ -1,82 +1,70 @@
-// import { useState } from "react"
-// import { useStore } from "../services/store"
-// import { useNavigate } from "react-router-dom"
+import { FaPeopleGroup } from "react-icons/fa6";
 import Orders from "./CustomersList"
 
-const stats = [
-  {
-    title: "Active Vehicles",
-    value: "20",
-    bg: "bg-indigo-50",
-    iconBg: "bg-indigo-100",
-    icon: (
-      <svg className="w-5 h-5 text-indigo-600" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-        <path strokeWidth="2" d="M3 13l2-5a2 2 0 012-1h10a2 2 0 012 1l2 5M5 13h14M5 13a2 2 0 100 4h1m12-4a2 2 0 110 4h-1" />
-      </svg>
-    ),
-  },
-  {
-    title: "Total Deliveries",
-    value: "15",
-    bg: "bg-orange-50",
-    iconBg: "bg-orange-100",
-    icon: (
-      <svg className="w-5 h-5 text-orange-600" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-        <path strokeWidth="2" d="M20 13V7a2 2 0 00-2-2H6a2 2 0 00-2 2v6m16 0l-8 5-8-5" />
-      </svg>
-    ),
-  },
-  {
-    title: "On-Time Rate",
-    value: "80.4%",
-    bg: "bg-sky-50",
-    iconBg: "bg-sky-100",
-    icon: (
-      <svg className="w-5 h-5 text-sky-600" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-        <path strokeWidth="2" d="M12 8v4l3 3M12 2a10 10 0 100 20 10 10 0 000-20z" />
-      </svg>
-    ),
-  },
-  {
-    title: "Fuel Efficiency",
-    value: "₱2,250",
-    bg: "bg-emerald-50",
-    iconBg: "bg-emerald-100",
-    icon: (
-      <svg className="w-5 h-5 text-emerald-600" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-        <path strokeWidth="2" d="M3 3h13v13H3zM16 7h3l2 2v7a2 2 0 01-2 2h-1" />
-      </svg>
-    ),
-  },
-]
 
 const ListDeliveries = () => {
 
   return (
     <div className="max-w-[1200px] mx-auto mt-8 space-y-6">
-      
-      {/* Stats Cards */}
-      {/* <div className="grid bg-white p-2 mx-6 rounded-2xl grid-cols-2 sm:grid-cols-2 lg:grid-cols-4 gap-4">
-        {stats.map((item, index) => (
-          <div
-            key={index}
-            className={`${item.bg} rounded-xl p-4 flex items-center justify-between`}
-          >
-            <div>
-              <p className="text-sm text-gray-500">{item.title}</p>
-              <h2 className="text-xl font-semibold text-gray-800">
-                {item.value}
-              </h2>
-            </div>
 
-            <div className={`${item.iconBg} p-2 rounded-lg`}>
-              {item.icon}
+      {/* Stats Cards */}
+      <div className="grid bg-white p-2 mx-6 rounded-2xl grid-cols-2 sm:grid-cols-2 lg:grid-cols-4 gap-4">
+      
+          {/* Active Vehicles */}
+          <div className="bg-indigo-50 rounded-xl p-4 flex items-center justify-between">
+            <div>
+              <p className="text-sm text-gray-500">Active Vehicles</p>
+              <h2 className="text-xl font-semibold text-gray-800">20</h2>
+            </div>
+            <div className="bg-indigo-100 p-2 rounded-lg">
+              <svg className="w-5 h-5 text-indigo-600" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                <path strokeWidth="2" d="M3 13l2-5a2 2 0 012-1h10a2 2 0 012 1l2 5M5 13h14M5 13a2 2 0 100 4h1m12-4a2 2 0 110 4h-1" />
+              </svg>
             </div>
           </div>
-        ))}
-      </div> */}
 
-      <Orders/>
+          {/* Total Deliveries */}
+          <div className="bg-orange-50 rounded-xl p-4 flex items-center justify-between">
+            <div>
+              <p className="text-sm text-gray-500">Total Deliveries</p>
+              <h2 className="text-xl font-semibold text-gray-800">15</h2>
+            </div>
+            <div className="bg-orange-100 p-2 rounded-lg">
+              <svg className="w-5 h-5 text-orange-600" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                <path strokeWidth="2" d="M20 13V7a2 2 0 00-2-2H6a2 2 0 00-2 2v6m16 0l-8 5-8-5" />
+              </svg>
+            </div>
+          </div>
+
+          {/* Total Customers */}
+          <div className="bg-sky-50 rounded-xl p-4 flex items-center justify-between">
+            <div>
+              <p className="text-sm text-gray-500">Total Customers</p>
+              <h2 className="text-xl font-semibold text-gray-800">9,210</h2>
+            </div>
+            <div className="bg-sky-100 p-2 rounded-lg">
+             <FaPeopleGroup />
+            </div>
+          </div>
+
+          {/* Fuel Efficiency */}
+          <div className="bg-emerald-50 rounded-xl p-4 flex items-center justify-between">
+            <div>
+              <p className="text-sm text-gray-500">Fuel Efficiency</p>
+              <h2 className="text-xl font-semibold text-gray-800">₱2,250</h2>
+            </div>
+            <div className="bg-emerald-100 p-2 rounded-lg">
+              <svg className="w-5 h-5 text-emerald-600" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                <path strokeWidth="2" d="M3 3h13v13H3zM16 7h3l2 2v7a2 2 0 01-2 2h-1" />
+              </svg>
+            </div>
+          </div>
+
+        
+
+      </div>
+
+      <Orders />
     </div>
   )
 }
