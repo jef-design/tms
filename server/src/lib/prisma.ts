406f3ef79bbf6cfe6c51e8f95880537247204121
jef-codesign <bermejojff97@gmail.com>
2026-01-14 14:44:21 +0800

    back  to prisma gen

--- server/src/lib/prisma.ts
+++ server/src/lib/prisma.ts
@@ -1,7 +1,6 @@
 import 'dotenv/config'
-
+import {PrismaClient} from '../generated/prisma/client.js'
 import { PrismaPg } from '@prisma/adapter-pg'
-import { PrismaClient } from '@prisma/client/extension'
 
 const pool = new PrismaPg({ connectionString: process.env.DATABASE_URL! })
 const prisma = new PrismaClient({adapter: pool})
